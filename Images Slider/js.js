23b48fb5fb4c776063eaf8e96b51391e16e2c3f0
Khaled Cheikhna <49081526+H-KC@users.noreply.github.com>
2022-03-04 16:29:08 +0500

    Add files via upload

---
+++ Images Slider/js.js
@@ -0,0 +1,23 @@
+let body = document.querySelector("body");
+let slider = document.querySelector(".slider");
+let arrows = document.querySelectorAll("button");
+
+let pics = [
+  "https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80",
+  "https://images.unsplash.com/photo-1495467033336-2effd8753d51?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80",
+  "https://images.unsplash.com/photo-1522735338363-cc7313be0ae0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2689&q=80",
+  "https://images.unsplash.com/photo-1559087867-ce4c91325525?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80",
+  "https://images.unsplash.com/photo-1511593358241-7eea1f3c84e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80",
+];
+let current = 0;
+
+arrows.forEach((arrow) => {
+  arrow.addEventListener("click", () => {
+    arrow.classList.contains("left") ? current-- : current++;
+    current < 0
+      ? (current = pics.length - 1)
+      : current > pics.length - 1 && (current = 0);
+    slider.style.backgroundImage = `url(${pics[current]})`;
+    body.style.backgroundImage = `url(${pics[current]})`;
+  });
+});
